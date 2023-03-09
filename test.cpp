c2fe5c6e04536d2d2fcf90c11b8ba575f4ba1d79
mjsdnz <121274756+mjsdnz@users.noreply.github.com>
2023-03-09 12:13:57 +0000

    Changes to be committed: 	modified:   test 	modified:   test.cpp

--- test.cpp
+++ test.cpp
@@ -1,31 +1,6 @@
-
 #include <iostream>
-
 using namespace std;
-#define BYTE unsigned char
-#define UINT unsigned int
-
-int main(int argc, char* argv[])
+int main()
 {
-
-	BYTE szCPU[16] = { 0 }; //定义存放CPU类型的数组
-	UINT uCPUID = 0U;       //定义存放CPU ID的数组
-	_asm                     //开始执行汇编
-	{
-		mov eax, 0 //获取CPU型号
-			cpuid
-			mov dword ptr szCPU[0], ebx
-			mov dword ptr szCPU[4], edx
-			mov dword ptr szCPU[8], ecx
-			mov eax, 1
-			cpuid
-			mov uCPUID, edx
-	}
-	cout << szCPU << endl;
-	cout << uCPUID << endl;
-
-
-	getchar();
-	return 0;
+    
 }
-
