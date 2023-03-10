8c137ac37e30bc9d0c93e99edf5afeee0f16d2df
mjsdnz <121274756+mjsdnz@users.noreply.github.com>
2023-03-10 06:02:15 +0000

    Changes to be committed: 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   out 	modified:   test 	modified:   test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+const int inf=1e9;
+int a;
 int main()
 {
-    
+    srand(clock());
+    freopen("out","w",stdout);
+    printf("100\n");
+    for(int i=1;i<=100;i++)
+    {
+        int a=rand();
+        while(a<-1*inf||a>inf)
+        {
+            a=rand();
+        }
+        printf("%d ",a);
+    }
+    return 0;
 }
